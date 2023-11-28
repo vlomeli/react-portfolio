d73aa4596c6e63a0d6f7356f7ee4d9f3afaeab9a
vlomeli <victorlomeli2023@gmail.com>
2023-11-27 20:07:21 -0800

    finished contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,40 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Contact() {
-    return <h1>Contact</h1>
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_w2spjch', 'template_xdtgrec', form.current, 'MAlFkml9eGdPHhKb4')
+        .then((result) => {
+            console.log(result.text);
+            console.log("message sent");
+            window.alert('Email has been sent!');
+            form.current.reset();
+        }, 
+        (error) => {
+            console.log(error.text);
+        });
+    };
+
+    return (
+    <>
+    <section className="contact">
+        <div className="contact-content">
+            <h1>CONTACT ME</h1>
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
+        </div>
+    </section>
+    </>
+    )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,7 @@ export default function Projects() {
     return (
         <>
         <div className='project-cards'>
+            <h1>MY PROJECTS</h1>
             <Card details={CardData} />
         </div>
         </>
