a3ab09270d5dfdac9eea85e0e30f581635cc099a
vlomeli <victorlomeli2023@gmail.com>
2024-05-07 23:06:27 -0700

    fixing journal img

--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -1,47 +1,54 @@
-import tictactoe from '../images/tictactoe.png'
-import emailtemp from '../images/emailtemp.png'
-import multiplication from '../images/multiplication.png'
-import weatherapp from '../images/weatherapp.png'
-import loginui from '../images/loginui.png'
-import reactweatherapp from '../images/reactweatherapp.png'
-import journal from '../images/journal.png';
+import journal from "../images/journal.png";
+import tictactoe from "../images/tictactoe.png";
+import emailtemp from "../images/emailtemp.png";
+import multiplication from "../images/multiplication.png";
+import weatherapp from "../images/weatherapp.png";
+import loginui from "../images/loginui.png";
+import reactweatherapp from "../images/reactweatherapp.png";
 
 const CardData = [
-    {
-        img: journal,
-        title: "journal",
-        description: "This is my first ever fullstack project. A journal with mood tracking and an interactive calendar.",
-    },
-    {
-        img: tictactoe,
-        title: "Tic-Tac-Toe",
-        description: "Tic Tac Toe game with win tracking and reset functionality. The main purpose of this project is to learn Javascript ",
-    },
-    {
-        img: emailtemp,
-        title: "Email Template",
-        description: "One of my very first projects at bay valley tech. A project used to work on my bootstrap grid.",
-    },
-    {
-        img: multiplication,
-        title: "Multiplication Sheet",
-        description: "A multiplication game that replicates the multiplication sheet we all used to do in grade school.",
-    },
-    {
-        img: weatherapp,
-        title: "Weather App",
-        description: "My first ever API project. Just a simple website that retrieves weather data from openweatherapi",
-    },
-    {
-        img: loginui,
-        title: "Login Page",
-        description: "A login page I created for Bay Valley Tech to work on my css and html skills.",
-    },
-    {
-        img: reactweatherapp,
-        title: "React Weather App",
-        description: "My second project working with an api, but taking my skill to a new level with react.",
-    },
+  {
+    img: journal,
+    title: "journal",
+    description:
+      "This is my first ever fullstack project. A journal with mood tracking and an interactive calendar.",
+  },
+  {
+    img: tictactoe,
+    title: "Tic-Tac-Toe",
+    description:
+      "Tic Tac Toe game with win tracking and reset functionality. The main purpose of this project is to learn Javascript ",
+  },
+  {
+    img: emailtemp,
+    title: "Email Template",
+    description:
+      "One of my very first projects at bay valley tech. A project used to work on my bootstrap grid.",
+  },
+  {
+    img: multiplication,
+    title: "Multiplication Sheet",
+    description:
+      "A multiplication game that replicates the multiplication sheet we all used to do in grade school.",
+  },
+  {
+    img: weatherapp,
+    title: "Weather App",
+    description:
+      "My first ever API project. Just a simple website that retrieves weather data from openweatherapi",
+  },
+  {
+    img: loginui,
+    title: "Login Page",
+    description:
+      "A login page I created for Bay Valley Tech to work on my css and html skills.",
+  },
+  {
+    img: reactweatherapp,
+    title: "React Weather App",
+    description:
+      "My second project working with an api, but taking my skill to a new level with react.",
+  },
 ];
 
 export default CardData;
