a236c782c7adbaca8bfb4faab48b0b1669397483
vlomeli <victorlomeli2023@gmail.com>
2024-05-17 17:27:52 -0700

    changes to about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,37 +2,45 @@ export default function About() {
     return (
       <>
         <section className="about">
+          <h1> ABOUT ME </h1>
           <div className="about-content">
-            <h1>ABOUT ME</h1>
-            <p>
-              I am a recent graduate of Stanislaus State University, where I
-              earned a Bachelor's degree in Business Administration with a
-              concentration in Computer Information Systems. Initially, I
-              believed Information Technology was the right path for me.
-              However, as I delved deeper into the field, I realized my true
-              passion lay elsewhere within the realm of computers. I love
-              working with computers and am passionate about creating.
-            </p>
-
-            <p>
-              This realization led me to explore web development further,
-              prompting me to enroll in a course with Bay Valley Tech, a
-              non-profit organization. Through this transformative experience, I
-              gained proficiency in HTML, CSS, JavaScript, React, Bootstrap, UI
-              Design, Node.js, MongoDB, SQL, Git, and developed a comprehensive
-              understanding of programming and software development.
-            </p>
-            <h1> GOALS</h1>
-            <p>
-              My goal is to continue honing my skills with the current tech
-              stack I am familiar with, enabling me to apply these skills
-              effectively in future job tasks. I am currently enrolled in Bay
-              Valley Tech's internship program, where I am applying my skill set
-              to real-world projects. Once the internship concludes, I aim to
-              secure a full-time position and advance within the industry. To
-              achieve this, I will remain actively involved, ask questions when
-              necessary, and eagerly participate in project discussions.
-            </p>
+            <div className="card2 about-card1">
+              <h1> MY START</h1>
+              <p>
+                I am a recent graduate of Stanislaus State University, where I
+                earned a Bachelor's degree in Business Administration with a
+                concentration in Computer Information Systems. Initially, I
+                believed Information Technology was the right path for me.
+                However, as I delved deeper into the field, I realized my true
+                passion lay elsewhere within the realm of computers. I love
+                working with computers and am eager to bring ideas to life.
+              </p>
+            </div>
+            <div className="card2 about-card2">
+              <h1>WHERE I AM NOW</h1>
+              <p>
+                This realization led me to explore web development further,
+                prompting me to enroll in a course with Bay Valley Tech, a
+                non-profit organization. Through this transformative experience,
+                I gained proficiency in HTML, CSS, JavaScript, React, Bootstrap,
+                UI Design, Node.js, MongoDB, SQL, Git, and developed a
+                comprehensive understanding of programming and software
+                development.
+              </p>
+            </div>
+            <div className="card2 about-card3">
+              <h1>GOAL</h1>
+              <p>
+                My goal is to continue honing my skills with the current tech
+                stack I am familiar with, enabling me to apply these skills
+                effectively in future job tasks. I am currently enrolled in Bay
+                Valley Tech's internship program, where I am applying my skill
+                set to real-world projects. Once the internship concludes, I aim
+                to secure a full-time position and advance within the industry.
+                To achieve this, I will remain actively involved, ask questions
+                when necessary, and eagerly participate in project discussions.
+              </p>
+            </div>
           </div>
         </section>
       </>
