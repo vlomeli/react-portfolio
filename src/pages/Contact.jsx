4ef7a70b2c05b7984494ae4b4ca139b6b86e4aef
vlomeli <victorlomeli2023@gmail.com>
2024-06-14 15:53:58 -0700

    changes to css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -20,21 +20,34 @@ export default function Contact() {
     };
 
     return (
-    <>
-    <section className="contact">
-        <div className="contact-content">
+      <>
+        <section className="contact">
+          <div className="contact-content">
             <h1>CONTACT ME</h1>
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
-        </form>
-        </div>
-    </section>
-    </>
-    )
+            <form ref={form} onSubmit={sendEmail}>
+              <label>Name</label>
+              <input type="text" name="user_name" />
+              <label>Email</label>
+              <input type="email" name="user_email" />
+              <label>Message</label>
+              <textarea name="message" />
+              <input type="submit" value="Send" />
+            </form>
+            <section className="contact-links">
+              <h1>vlponcebusiness@gmail.com</h1>
+              <div className="links-row">
+                <h3>
+                  <a href="https://www.linkedin.com/in/vlomeliponce/">
+                    LinkedIn
+                  </a>
+                </h3>
+                <h3>
+                  <a href="https://github.com/vlomeli">GitHub</a>
+                </h3>
+              </div>
+            </section>
+          </div>
+        </section>
+      </>
+    );
 }
