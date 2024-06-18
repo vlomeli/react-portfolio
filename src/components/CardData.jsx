93343a3872f1b7b232e093685634e96523ab8f2b
vlomeli <victorlomeli2023@gmail.com>
2024-06-18 14:52:21 -0700

    changes to urls

--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -16,14 +16,14 @@ const CardData = [
     title: "Full-stack Social Media",
     description:
       "During my time at BVT, my team and I developed a full-stack social media application using React, Bulma, MongoDB, and Express.js. This platform enables users to share text-based posts and express themselves, fostering engagement and interaction among the community.",
-    //githubURL: "https://github.com/FrancescaImmediato/dev-duckies-sm-proj-front", "https://github.com/JJeshua/ddsm-back-end",
+    //githubURL: "https://github.com/vlomeli/ddsm-front-end", "https://github.com/vlomeli/ddsm-back-end",
   },
   {
     img: testingtool,
     title: "Python Testing Tool",
     description:
       "My team and I developed a Python testing tool designed to rigorously stress test our social media application's endpoints. Leveraging pytest for testing and faker for generating dummy data, this mini project aimed to cover a wide range of scenarios and edge cases, ensuring robust performance and reliability.",
-    //githubURL: "https://github.com/JJeshua/ddsm-backend-testing-tool",
+    //githubURL: "https://github.com/vlomeli/ddsm-backend-testing-tool",
   },
   {
     img: journal,
